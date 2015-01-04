21efdbbdef06ec2f1b73c78a66739ca46a228b9f
Chris Garrett <cmg@pzuraq.co>
2015-01-03 20:22:48 -0800

    fixing blueprints

--- blueprints/ember-inputmask/index.js
+++ blueprints/ember-inputmask/index.js
@@ -1,6 +1,9 @@
 module.exports = {
-    normalizeEntityName: function() {},
-    afterInstall: function(options) {
-        return this.addBowerPackageToProject('jquery.inputmask', '3.1.49');
-    }
+  name: 'ember-inputmask',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import('bower_components/jquery.inputmask');
+  }
 };
