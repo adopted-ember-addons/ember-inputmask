e2fb588a90f61943e42c3474d43e727b86b34f3b
Chris Garrett <cmg@pzuraq.co>
2015-01-03 20:33:19 -0800

    fixing index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-inputmask'
+  name: 'ember-inputmask',
   included: function(app) {
     this._super.included(app);
     app.import(app.bowerDirectory + '/jquery.inputmask/dist/jquery.inputmask.bundle.min.js');
