f78a7c4ff246b584bbd4cb13fc1b2ac3d16d33c2
Tushar Chauhan <chauhan.tushar@gmail.com>
2016-03-09 19:04:45 +0530

    Cleaned up and updated email-input test

--- tests/unit/components/email-input-test.js
+++ tests/unit/components/email-input-test.js
@@ -13,10 +13,10 @@ moduleForComponent('email-input', 'email-input component', {
   unit: true
 });
 
-test('masked values are correct', function(assert) {
-  assert.expect(1);
+test('values are correct', function(assert) {
+  assert.expect(2);
 
-  this.subject();
+  var component = this.subject();
 
   // append the component to the DOM
   this.render();
@@ -26,25 +26,6 @@ test('masked values are correct', function(assert) {
   triggerEvent('input', 'blur');
   andThen(function() { // wait for async helpers to complete
     assert.equal(find('input').val(), 'test@test.test');
+    assert.equal(component.get('unmaskedValue'), 'test@test.test');
   });
 });
-
-// Test below is failing because of wrong assertion of unmaskedValue
-// The unmasked value in the case of email-input will always be same as
-// masked input.
-
-// test('unmasked values are correct', function(assert) {
-//   assert.expect(1);
-//
-//   var component = this.subject();
-//
-//   // append the component to the DOM
-//   this.render();
-//
-//   // testing default value
-//   fillIn('input', 'test@test.test');
-//   triggerEvent('input', 'blur');
-//   andThen(function() { // wait for async helpers to complete
-//     assert.equal(component.get('unmaskedValue'), 'testtesttest');
-//   });
-// });
