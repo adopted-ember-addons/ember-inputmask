17b5d130e6aae6a04fc6cf2d6f8adf1b85f4bfc5
Lei Zhao <leizhao4@gmail.com>
2017-03-24 00:03:04 -0500

    Replace funnel with ember-cli-node-assets

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
 /* eslint-env node */
 'use strict';
 
-var path = require('path');
-var Funnel = require('broccoli-funnel');
-var MergeTrees = require('broccoli-merge-trees');
-
 module.exports = {
   name: 'ember-inputmask',
-  included: function(app) {
-    this._super.included.apply(this, arguments);
-    while (typeof app.import !== 'function' && app.app) {
-      app = app.app;
+  options: {
+    nodeAssets: {
+      'jquery.inputmask': {
+        vendor: [
+          'extra/dependencyLibs/inputmask.dependencyLib.js',
+          'dist/inputmask/inputmask.js',
+          'dist/inputmask/inputmask.extensions.js',
+          'dist/inputmask/inputmask.date.extensions.js',
+          'dist/inputmask/inputmask.numeric.extensions.js',
+          'dist/inputmask/inputmask.phone.extensions.js',
+          'dist/inputmask/inputmask.regex.extensions.js'
+        ]
+      }
     }
-    app.import('vendor/inputmask/extra/dependencyLibs/inputmask.dependencyLib.js');
-    app.import('vendor/inputmask/dist/inputmask/inputmask.js');
-    app.import('vendor/inputmask/dist/inputmask/inputmask.extensions.js');
-    app.import('vendor/inputmask/dist/inputmask/inputmask.date.extensions.js');
-    app.import('vendor/inputmask/dist/inputmask/inputmask.numeric.extensions.js');
-    app.import('vendor/inputmask/dist/inputmask/inputmask.phone.extensions.js');
-    app.import('vendor/inputmask/dist/inputmask/inputmask.regex.extensions.js');
   },
-  treeForVendor: function(vendorTree) {
-    var inputmaskPath = path.dirname(require.resolve('jquery.inputmask'));
-    var inputmaskTree = new Funnel(inputmaskPath, {
-      files: ['extra/dependencyLibs/inputmask.dependencyLib.js',
-        'dist/inputmask/inputmask.js',
-        'dist/inputmask/inputmask.extensions.js',
-        'dist/inputmask/inputmask.date.extensions.js',
-        'dist/inputmask/inputmask.numeric.extensions.js',
-        'dist/inputmask/inputmask.phone.extensions.js',
-        'dist/inputmask/inputmask.regex.extensions.js'],
-      destDir: '/inputmask'
-    });
-    var trees = [vendorTree, inputmaskTree];
-    return new MergeTrees(trees, {
-      annotation: 'ember-inputmask: treeForVendor'
-    });
+  included() {
+    this._super.included.apply(this, arguments);
+    this.import('vendor/jquery.inputmask/extra/dependencyLibs/inputmask.dependencyLib.js');
+    this.import('vendor/jquery.inputmask/dist/inputmask/inputmask.js');
+    this.import('vendor/jquery.inputmask/dist/inputmask/inputmask.extensions.js');
+    this.import('vendor/jquery.inputmask/dist/inputmask/inputmask.date.extensions.js');
+    this.import('vendor/jquery.inputmask/dist/inputmask/inputmask.numeric.extensions.js');
+    this.import('vendor/jquery.inputmask/dist/inputmask/inputmask.phone.extensions.js');
+    this.import('vendor/jquery.inputmask/dist/inputmask/inputmask.regex.extensions.js');
   }
 };
