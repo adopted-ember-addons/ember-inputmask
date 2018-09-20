e1cf1330281e75d6cb86104cab63b8e9591587ad
brandynbennett <brandynlbennett@gmail.com>
2018-09-19 20:57:47 -0600

    Clean up one-way-email-mask yuidoc

--- addon/components/one-way-email-mask.js
+++ addon/components/one-way-email-mask.js
@@ -1,10 +1,6 @@
 import OneWayInputMask from 'ember-inputmask/components/one-way-input-mask';
 
 /**
- * `{{one-way-email-mask}}` component.
- *
- * Displays an input that masks to email
- *
  * @class OneWayEmailMask
  */
 export default OneWayInputMask.extend({
