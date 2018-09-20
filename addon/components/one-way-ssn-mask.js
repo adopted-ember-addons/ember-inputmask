10e1c5a7986c784a4d4638160e1e76ae2249f51d
brandynbennett <brandynlbennett@gmail.com>
2018-09-19 21:08:06 -0600

    Clean up one-way-ssn-mask yuidoc

--- addon/components/one-way-ssn-mask.js
+++ addon/components/one-way-ssn-mask.js
@@ -1,9 +1,6 @@
 import OneWayInputMask from 'ember-inputmask/components/one-way-input-mask';
 
 /**
- * `{{one-way-ssn-mask}}` component
- *  Displays an input that masks SSN format
- *
  * @class OneWaySSNMask
  */
 export default OneWayInputMask.extend({
