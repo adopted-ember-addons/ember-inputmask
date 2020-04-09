2f1a4408cd7aef1c5e4153189548351db8d13ebc
brandynbennett <brandynlbennett@gmail.com>
2020-04-08 22:50:33 -0600

    specify yarn in try scenario

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,6 +3,7 @@
 const getChannelURL = require('ember-source-channel-url');
 module.exports = async function() {
   return {
+    useYarn: true,
     scenarios: [
       {
         name: 'ember-lts-3.12',
