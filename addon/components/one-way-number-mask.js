ac07c153cf3dd51b6900ad1cf05138625588178d
Robert Wagner <rwwagner90@gmail.com>
2025-11-21 07:00:08 -0500

    Update one-way-number-mask.js

--- addon/components/one-way-number-mask.js
+++ addon/components/one-way-number-mask.js
@@ -18,12 +18,6 @@ export default class OneWayNumberMask extends OneWayInputMask {
   NON_ATTRIBUTE_BOUND_PROPS = DEFAULT_NON_BOUND_PROPS.concat('decimal');
 
   /**
-   * @field mask
-   * @override
-   */
-  mask = 'integer';
-
-  /**
    * Set this to true to include decimals
    *
    * @argument decimal
@@ -36,11 +30,25 @@ export default class OneWayNumberMask extends OneWayInputMask {
 
     set(this, '_options', Object.assign({}, this._options, DEFAULT_OPTIONS));
 
-    if (this.decimal) {
-      set(this, 'mask', 'decimal');
+    // Set mask if not explicitly provided
+    if (isBlank(this.mask)) {
+      set(this, 'mask', this.decimal ? 'decimal' : 'integer');
+    }
 
+    if (this.decimal) {
       // Give default digits if we don't have them already
       if (!this.options || isBlank(this.options.digits)) {
+        set(this, '_options.digits', 2);
+      }
+    }
+  }
+
+  didReceiveAttrs() {
+    super.didReceiveAttrs();
+
+    // Ensure digits is set when decimal is true and not explicitly provided
+    if (this.decimal && (!this.options || isBlank(this.options.digits))) {
+      if (isBlank(this._options.digits)) {
         set(this, '_options.digits', 2);
       }
     }
